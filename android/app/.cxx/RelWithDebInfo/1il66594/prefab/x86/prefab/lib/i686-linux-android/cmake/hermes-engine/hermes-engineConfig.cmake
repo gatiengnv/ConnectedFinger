d81e3715af7c43596ecd5e363cf9bbeef97e6261
Gatien <gatien.gen51@gmail.com>
2025-12-17 14:14:06 +0100

    Add arduino code and app apk

---
+++ android/app/.cxx/RelWithDebInfo/1il66594/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "/Users/gatiengenevois/.gradle/caches/8.13/transforms/352fe9a1af5567ea5b9c46d165b47ecf/transformed/hermes-android-0.82.0-release/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/gatiengenevois/.gradle/caches/8.13/transforms/352fe9a1af5567ea5b9c46d165b47ecf/transformed/hermes-android-0.82.0-release/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
